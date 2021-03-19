3e11067465f26dda2ad3d8ab56b8da19e3ddfb8f
yuki <tokuakeyuki@gmail.com>
2021-03-19 12:03:55 +0000

    first dev

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,8 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  mode: 'universal',
+  srcDir: 'src',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - hamuhouse',
